dff875875d6943e34dcc7ba52b3e0e2b4708b389
Lorenzo Corbella <lcorbella@expertsystem.local>
2019-09-19 09:50:31 +0200

    sync to 0.2.2

--- src/index.js
+++ src/index.js
@@ -18,17 +18,31 @@ window.onload = function () {
 
     const app = new Luce(root, { debug: true });   // passing the root and a config obj
 
-    // registering components
+    // (required) registering components
     app.addComponent('dad-component', dadCtrl)
         .addComponent('child-component', childCtrl)
         .addComponent('shared-component', sharedCtrl)
         .addComponent('about-component', aboutCtrl)
         .addComponent('not-found-component', notFoundCtrl)
 
-    // rendering the root (no ROUTER)
-    // app.rootRender(root, 'dad-component');
+    // (optional) run config middlewares
+    app.use((luce, next)=>{
+            console.log('First middleware running...');
+            const config = {
+                URL: 'localhost:3000',
+                language:'en',
+                availableLanguages:['en','ita']
+            };
+            luce.plug('config', config); // .plug() injects a $<name> obj in each component (in this case $config)
+    
+            next(); // to advance to the next middleware
+        })
+        .use((luce, next)=>{
+            console.log('Second middleware running...');
+            next();
+        });
 
-    // rendering the root with FE ROUTER
+    // (optional) set the  ROUTER
     app.router
         // page fade animation 
         .beforeChange((luce) => luce.main.classList.add('fade'))
@@ -37,6 +51,8 @@ window.onload = function () {
         .addRoute('/', 'dad-component')
         .addRoute('/about', 'about-component')
         .addRoute('/about/:id/:counter', 'about-component')
-        .ifNotFound('not-found-component')
-        .start()
+        .ifNotFound('not-found-component');
+    
+    // start the app
+    app.init();
 }
